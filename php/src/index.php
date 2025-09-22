cad9adf71ad6da9c9794f12a2a917db80244c8ca
Javier Llorens <javllogos@alu.edu.gva.es>
2025-09-22 15:30:41 +0200

    Cambiado indice para acceder a las actividades

--- php/src/index.php
+++ php/src/index.php
@@ -2,6 +2,13 @@
 <html>
 <body>
     <h1>Benvingut a la meva web</h1>
-    <p>La data d'avui és: <?= date('Y-m-d') ?></p>
+    <p>La data d'avui és: <?= date('Y-m-d') ?></p><br>
+    <a href="actividades/1.php">Activitat 1</a><br>
+    <a href="actividades/2.php">Activitat 2</a><br>
+    <a href="actividades/3.php">Activitat 3</a><br>
+    <a href="actividades/4.php">Activitat 4</a><br>
+    <a href="actividades/5.php">Activitat 5</a><br>
+    <a href="actividades/6.php">Activitat 6</a><br>
+    <a href="actividades/7.php">Activitat 7</a><br>
 </body>
 </html>
